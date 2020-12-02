99e93e3c9354cf115fbee0f4dc202a976036b985
Qadroid <34576771+Qadroid@users.noreply.github.com>
2020-12-02 13:48:19 +0000

    Adding solution for first puzzle

---
+++ Day 1/day-1.py
@@ -0,0 +1,23 @@
+
+inputfile = open("input.txt")
+inputlist = []
+
+for line in inputfile:
+    line = line.strip()
+    inputlist.append(line)
+
+inputlist = list(map(int, inputlist))
+
+print(inputlist)
+
+num1 = 0
+num2 = 0
+
+for i in inputlist:
+    for i2 in inputlist:
+        if i + i2 == 2020:
+            print("First number is equal to: ", i)
+            print("Second number is equal to: ", i2)
+            print("Product of both numbers is equal to: ", i * i2)
+
+inputfile.close()
